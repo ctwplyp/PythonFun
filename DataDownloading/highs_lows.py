5fe2e1cbb278225178374cb4dadd74df797a6b6d
Sean <ctwplyp@gmail.com>
2018-11-10 08:52:11 -0800

    f/DataDownloading: Create graph of high temps

--- DataDownloading/highs_lows.py
+++ DataDownloading/highs_lows.py
@@ -1,9 +1,26 @@
 import csv
 
+from matplotlib import pyplot as plt
+
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 
-	for i, column_header in enumerate(header_row):
-		print(i, column_header)
+#	for i, column_header in enumerate(header_row):
+#		print(i, column_header)
+
+	highs =[]
+	for row in reader:
+		high =int(row[1])
+		highs.append(high)
+
+fig =plt.figure(dpi=128, figsize=(10,6))
+plt.plot(highs, c='red')
+
+plt.title('Daily high temps, Jul 2014', fontsize = 24)
+plt.xlabel('', fontsize =16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
