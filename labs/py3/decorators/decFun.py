b44a55d001adf94810dcc5ca2aa62ed35030e97f
Sean <ctwplyp@gmail.com>
2019-01-09 16:10:33 -0800

    f/tutorial: Memoize practicing

---
+++ labs/py3/decorators/decFun.py
@@ -0,0 +1,12 @@
+def add(increment):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            return increment + func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@add(3)
+def f(n):
+    return n+2
+
+print(f(4))
